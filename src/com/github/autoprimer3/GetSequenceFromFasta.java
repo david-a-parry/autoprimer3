6a0dc73216b4b9d270967e7a7ac0301be214dd0c
David A. Parry <david.parry@igmm.ed.ac.uk>
2014-10-29 12:56:05 +0000

    added GPLv3 license to source files

--- src/com/github/autoprimer3/GetSequenceFromFasta.java
+++ src/com/github/autoprimer3/GetSequenceFromFasta.java
@@ -1,24 +1,18 @@
 /*
-* Copyright (C) 2013 David A. Parry
- * d.a.parry@leeds.ac.uk
- * https://sourceforge.net/projects/...
- * 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*this class leverages some useful features from Picard <http://sourceforge.net/projects/picard/>
- * to quickly extract sequences from an indexed fasta file within given coordinates
+ * Copyright (C) 2014 David A. Parry <d.a.parry@leeds.ac.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.github.autoprimer3;
