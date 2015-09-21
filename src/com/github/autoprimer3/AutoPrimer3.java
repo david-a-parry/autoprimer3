cdae1f6cd826d7c9340376414b122857c95e8c97
David A. Parry <gantzgraf@github.com>
2015-09-21 10:27:49 +0100

    Catch retrieveSequence errors in main class
    Allow for a slightly more informative error message when an
    exception is encountered retrieving DNA from DAS.

--- src/com/github/autoprimer3/AutoPrimer3.java
+++ src/com/github/autoprimer3/AutoPrimer3.java
@@ -1542,8 +1542,26 @@ public class AutoPrimer3 extends Application implements Initializable{
                         + end);
                 updateMessage("Retrieving DNA for region " + n + 
                         " of " + regions.size() + "...");
-                String dna = seqFromDas.retrieveSequence(
+                String dna;
+                try {
+                    dna= seqFromDas.retrieveSequence(
                         genome, r.getChromosome(), start, end);
+                }catch(DocumentException | MalformedURLException seqex){
+                    final String rString = r.getChromosome() + ":" + start + "-" + end;
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            Dialogs seqError = 
+                                    Dialogs.create().title("Error").
+                                    masthead("Error retrieving DNA").
+                                    message("Exception encountered while retrieving DNA for"
+                                            + "region " + rString + ". See below:")
+                                    .styleClass(Dialog.STYLE_CLASS_NATIVE);
+                                seqError.showException(seqex);
+                        }
+                    });
+                    return null;
+                }
                 updateProgress(++p, regions.size() * 3);
                 //System.out.println(dna);//debug only
                 String snpDb = (String) snpsChoiceBox.getSelectionModel().getSelectedItem();
@@ -1959,8 +1977,30 @@ public class AutoPrimer3 extends Application implements Initializable{
                             HashMap<String, String> checkedTranscript = new HashMap<>();
                                 //only want to check names/ID has already been 
                                 //used once per region i.e. for previous regions
-                            String dna = seqFromDas.retrieveSequence(
+                            String dna;
+                            try{
+                                dna = seqFromDas.retrieveSequence(
                                     genome, r.getChromosome(), r.getStartPos(), r.getEndPos());
+                            }catch (DocumentException | MalformedURLException seqex){
+                                final String rString = r.getChromosome() + ":" + 
+                                                    r.getStartPos() + "-" + r.getEndPos();
+                                final String rName = r.getName();
+                                Platform.runLater(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            
+                                            Dialogs seqError = 
+                                                Dialogs.create().title("Error").
+                                                masthead("Error retrieving DNA").
+                                                message("Exception encountered while retrieving DNA for"
+                                                        + "region " + rString + " for gene " 
+                                                        + rName + ". See below:")
+                                                .styleClass(Dialog.STYLE_CLASS_NATIVE);
+                                            seqError.showException(seqex);
+                                        }
+                                });
+                                return null;
+                            }
                             //System.out.println(dna);//debug only
                             String snpDb = (String) snpsChoiceBox.getSelectionModel().getSelectedItem();
                             ArrayList<GenomicRegionSummary> snps = new ArrayList<>();
--- src/com/github/autoprimer3/SequenceFromDasUcsc.java
+++ src/com/github/autoprimer3/SequenceFromDasUcsc.java
@@ -53,51 +53,46 @@ public class SequenceFromDasUcsc {
         }
     }
     //uses 0-based coordinates for compatibility with gene tables, bed etc., although DAS uses 1-based
-    public String retrieveSequence(String build, String chrom, Integer start, Integer end){
+    public String retrieveSequence(String build, String chrom, Integer start, Integer end)
+                                throws DocumentException, MalformedURLException{
         if (! buildToMapMaster.containsKey(build)){
             return null;
         }else{
-            try{
-                String chromNumber = chrom.replaceFirst("chr", "");
-                int length = 0;
-                URL entryPointUrl = new URL(buildToMapMaster.get(build) + "/entry_points");
-                Document dasXml;
-                SAXReader reader = new SAXReader();
-                dasXml  = reader.read(entryPointUrl);
-                Element root = dasXml.getRootElement();
-                for ( Iterator i = root.elementIterator( "ENTRY_POINTS" ); i.hasNext(); ) {
-                    Element dsn = (Element) i.next();
-                    for (Iterator j = dsn.elementIterator("SEGMENT"); j.hasNext();){
-                        Element seg = (Element) j.next();
-                        String id = seg.attributeValue("id");
-                        if (id != null && id.equals(chromNumber)){
-                            String stop = seg.attributeValue("stop");
-                            length = Integer.valueOf(stop);
-                            break;
-                        }
+            String chromNumber = chrom.replaceFirst("chr", "");
+            int length = 0;
+            URL entryPointUrl = new URL(buildToMapMaster.get(build) + "/entry_points");
+            Document dasXml;
+            SAXReader reader = new SAXReader();
+            dasXml  = reader.read(entryPointUrl);
+            Element root = dasXml.getRootElement();
+            for ( Iterator i = root.elementIterator( "ENTRY_POINTS" ); i.hasNext(); ) {
+                Element dsn = (Element) i.next();
+                for (Iterator j = dsn.elementIterator("SEGMENT"); j.hasNext();){
+                    Element seg = (Element) j.next();
+                    String id = seg.attributeValue("id");
+                    if (id != null && id.equals(chromNumber)){
+                        String stop = seg.attributeValue("stop");
+                        length = Integer.valueOf(stop);
+                        break;
                     }
                 }
-                if (length > 0){
-                    end = end <= length  ? end : length;    
-                }
-                StringBuilder dna = new StringBuilder();
-                URL genomeUrl = new URL(buildToMapMaster.get(build) + 
-                        "/dna?segment="+chrom + ":" + (start + 1) + "," + end);
-                dasXml  = reader.read(genomeUrl);
-                root = dasXml.getRootElement();
-                for ( Iterator i = root.elementIterator( "SEQUENCE" ); i.hasNext(); ) {
-                    Element dsn = (Element) i.next();
-                    Element seq = dsn.element("DNA");
-                    String text = seq.getText().replaceAll("\n", "");
-                    dna.append(text);
-                    //dna.append(seq.getText());
-                }
-                return dna.toString();
-            }catch(DocumentException|MalformedURLException ex){
-                //TO DO - handle (throw) this error properly
-                ex.printStackTrace();
-                return null;
             }
+            if (length > 0){
+                end = end <= length  ? end : length;    
+            }
+            StringBuilder dna = new StringBuilder();
+            URL genomeUrl = new URL(buildToMapMaster.get(build) + 
+                    "/dna?segment="+chrom + ":" + (start + 1) + "," + end);
+            dasXml  = reader.read(genomeUrl);
+            root = dasXml.getRootElement();
+            for ( Iterator i = root.elementIterator( "SEQUENCE" ); i.hasNext(); ) {
+                Element dsn = (Element) i.next();
+                Element seq = dsn.element("DNA");
+                String text = seq.getText().replaceAll("\n", "");
+                dna.append(text);
+                //dna.append(seq.getText());
+            }
+            return dna.toString();
             
         }
         
