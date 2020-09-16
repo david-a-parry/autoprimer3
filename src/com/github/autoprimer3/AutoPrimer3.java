8b2fa2d4e0df984ed96481aee3aaf56569e27fa9
David A. Parry <david.parry@igmm.ed.ac.uk>
2020-09-16 14:22:54 +0100

    Update version

--- src/com/github/autoprimer3/AutoPrimer3.java
+++ src/com/github/autoprimer3/AutoPrimer3.java
@@ -101,7 +101,7 @@ import org.dom4j.DocumentException;
  */
 public class AutoPrimer3 extends Application implements Initializable{
     
-    String VERSION = "3.0.3";
+    String VERSION = "3.1";
     
     @FXML
     AnchorPane mainPane;
